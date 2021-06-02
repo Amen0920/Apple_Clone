38e0ceac671b0157b6ba2ea3342b14eb9153a1b6
Amen0920 <luj0605@gmail.com>
2021-06-02 18:00:20 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
     let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset) 보다 이전에 위치한 스크롤 섹션들의 높이합
     let currentScene = 0; // 현재 활성화된 씬 (scroll-section);
 
+    let enterNewScene = false; // 새로운 scene이 시작되는 순간 true
+
     const scenInfo = [
         {
             // 0
@@ -17,7 +19,11 @@
                 messageD :document.querySelector('#scroll-section-0 .main-message.d'),
             },
             values:{
-                messageA_oppacity: [0,1],
+                messageA_opacity: [0,1, {start:0.1, end:0.2}],
+                messageB_opacity: [0,1, {start:0.3, end:0.4}],
+                messageC_opacity: [0,1, {start:0.5, end:0.6}],
+                messageC_opacity: [0,1, {start:0.7, end:0.8}],
+                
 
             }
         },
@@ -74,9 +80,26 @@
     }
     function calcValues (values,currentYOffset) {
         let rv; 
+        const scrollHeight = scenInfo[currentScene].scrollHeight;
         // 현재 씬에서 스크롤된 범위를 비율로 구하기.
-        let scrollRatio = currentYOffset / scenInfo[currentScene].scrollHeight;
-        rv = parseInt(scrollRatio) * 300; 
+        const scrollRatio = currentYOffset / scrollHeight;
+
+        if(values[2]){
+            // start end 가 있는 경우. start~end 사이에 animation 실행.
+            const partScrollStart = values[2].start * scrollHeight;
+            const partScrollEnd = values[2].end * scrollHeight;
+            const partScrollHeight = partScrollEnd - partScrollStart;
+            if(currentYOffset < partScrollStart){ 
+                rv = values[0];
+            }else if(currentYOffset > partScrollEnd){
+                rv = values[1];
+            }else{
+                rv = (currentYOffset-partScrollStart)/partScrollHeight * (values[1] - values[0]) + values[0]; 
+            }
+        }else{
+            rv = scrollRatio * (values[1] - values[0]) + values[0]; 
+        }
+       
         return rv;
     }
 
@@ -84,13 +107,13 @@
         const objs = scenInfo[currentScene].objs;
         const values = scenInfo[currentScene].values;
         const currentYOffset =  yOffset-prevScrollHeight;
-
-        console.log(currentScene, currentYOffset)
+      
+       
         switch (currentScene) {
             case 0 : 
-                let messageA_oppacity_0 = values.messageA_oppacity[0];
-                let messageA_oppacity_1 = values.messageA_oppacity[1];
-                calcValues(values.messageA_oppacity, currentYOffset);
+                let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset);
+                objs.messageA.style.opacity = messageA_opacity_in;
+                console.log('opacity ratio : ',messageA_opacity_in)
                 break;
             case 1 : 
                 break;
@@ -103,6 +126,7 @@
     }
     
     function scrollLoop() {
+        enterNewScene = false;
         prevScrollHeight = 0;
         for(let i = 0; i < currentScene; i++){
             prevScrollHeight += scenInfo[i].scrollHeight;
@@ -111,15 +135,18 @@
         // currentScene 이 바뀔때만 body태그 아이디 변경해줌.
         if(yOffset > prevScrollHeight + scenInfo[currentScene].scrollHeight) {
             if(currentScene == 3) return;
+            enterNewScene = true;
             currentScene++;
             document.body.setAttribute('id',`show-scene-${currentScene}`)
         }
         if(yOffset < prevScrollHeight){
             if(currentScene == 0)return;
+            enterNewScene = true;
             currentScene--;
             document.body.setAttribute('id',`show-scene-${currentScene}`)
         }
 
+        if(enterNewScene) return;
         playAnimation();
     }
  
